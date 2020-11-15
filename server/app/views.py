f8b7181784a538449a022dd3415c1fc24c3e8136
Markus <Markus.Mueller@kameralamt.de>
2020-11-15 18:49:06 +0100

    Homepage ferstigfestellt und neue Seiten erstellt

--- server/app/views.py
+++ server/app/views.py
@@ -8,6 +8,18 @@ from app import app
 def index():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/Wohnzimmer.html')
+def Wohnzimmer():
+    return render_template("Wohnzimmer.html")
+
+@app.route('/Schlafzimmer.html')
+def Schlafzimmer():
+    return render_template("Schlafzimmer.html")
+
+@app.route('/Flur_Eingang.html')
+def Flur_Eingang():
+    return render_template("Flur_Eingang.html")
+
+@app.route('/Flur_Bad.html')
+def Flur_Bad():
+    return render_template("Flur_Bad.html")
